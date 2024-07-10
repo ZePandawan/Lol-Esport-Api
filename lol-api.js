b90374f1cfb1f4d636632634e3cc31b3fcd31d7f
Vincent_Reix_54 <vincentreix54@gmail.com>
2024-07-10 15:47:39 +0200

    Last changes

--- lol-api.js
+++ lol-api.js
@@ -2,147 +2,174 @@
 //const fetch = require('node-fetch');
 import express from 'express';
 import fetch from 'node-fetch';
+import prompt from 'prompt';
 
 
 const app = express();
-const PORT = 6969;
+let PORT = 6969;
 
-app.use(express.json());
+prompt.start();
+prompt.message = "";
+prompt.delimiter = "";
+prompt.get([
+  {
+    name: 'port',
+    description: '"\x1b[32mQuel port voulez-vous utiliser ? (default: 6969)\n > '
+  }
+], function(err, res){
 
-// #########################################################################################################################
-// ##################################################### DEFAULT ROUTE #####################################################
-// #########################################################################################################################
-app.get('/', (req, res) => {
+  try{
+    res.port = parseInt(res.port);
+  }catch(e){
+    res.port = 6969;
+  }
+
+  if(!err && res.port && typeof res.port === 'number' && res.port > 0 && res.port < 65535){
+    PORT = res.port;
+  }
+  init();
+});
+
+
+function init(){
+
+    app.use(express.json());
+    
+  // #########################################################################################################################
+  // ##################################################### DEFAULT ROUTE #####################################################
+  // #########################################################################################################################
+  app.get('/', (req, res) => {
     res.json({
-        name: 'LoL API',
-        version: "0.1.0",
-        author: "Vincent REIX <vincentreix54@gmail.com>",
-        repository: ""
+      name: 'LoL API',
+      version: "0.1.0",
+      author: "Vincent REIX <vincentreix54@gmail.com>",
+      repository: ""
     });
-});
-// #########################################################################################################################
+  });
+  // #########################################################################################################################
 
 
 
-// #########################################################################################################################
-// ##################################################### LEAGUES ROUTES ####################################################
-// #########################################################################################################################
-app.get('/leagues', async (req,res) => {
-  const data = await requestData(`getLeagues?hl=fr-FR`);
-  res.json(data);
-});
+  // #########################################################################################################################
+  // ##################################################### LEAGUES ROUTES ####################################################
+  // #########################################################################################################################
+  app.get('/leagues', async (req,res) => {
+    const data = await requestData(`getLeagues?hl=fr-FR`);
+    res.json(data);
+  });
 
-app.get('/league/id/:id', async (req,res) => {
-  const leagueId = req.params.id;
-  const data = await requestData(`getLeagues?hl=fr-FR`);
-  const leagues = data.data.leagues;
-  const league = leagues.find(l => l.id == leagueId);
-  res.json(league);
-});
+  app.get('/league/id/:id', async (req,res) => {
+    const leagueId = req.params.id;
+    const data = await requestData(`getLeagues?hl=fr-FR`);
+    const leagues = data.data.leagues;
+    const league = leagues.find(l => l.id == leagueId);
+    res.json(league);
+  });
 
-app.get('/league/name/:name', async (req,res) => {
-  const leagueName = req.params.name;
-  const data = await requestData(`getLeagues?hl=fr-FR`);
-  const leagues = data.data.leagues;
-  const league = leagues.find(l => l.name == leagueName);
-  res.json(league);
-});
-// #########################################################################################################################
-
-
-
-// #########################################################################################################################
-// ################################################### TOURNAMENTS ROUTES ##################################################
-// #########################################################################################################################
-app.get('/tournaments', async (req,res) => {
-  const data = await requestData(`getCompletedEvents?hl=fr-FR`);
-  const tournaments = data.data.schedule.events;
-  res.json(tournaments);
-});
-
-app.get('/tournaments/league/:id', async (req,res) => {
-  const leagueId = req.params.id;
-  const data = await requestData(`getTournamentsForLeague?hl=fr-FR&leagueId=${leagueId}`);
-  res.json(data);
-});
-
-app.get('/tournament/:id', async (req,res) => {
-  const tournamentId = req.params.id;
-  const data = await requestData(`getStandings?hl=fr-FR&tournamentId=${tournamentId}`);
-  res.json(data);
-});
-// #########################################################################################################################
+  app.get('/league/name/:name', async (req,res) => {
+    const leagueName = req.params.name;
+    const data = await requestData(`getLeagues?hl=fr-FR`);
+    const leagues = data.data.leagues;
+    const league = leagues.find(l => l.name == leagueName);
+    res.json(league);
+  });
+  // #########################################################################################################################
 
 
 
-// #########################################################################################################################
-// ###################################################### TEAMS ROUTES #####################################################
-// #########################################################################################################################
-app.get('/teams', async (req,res) => {
-  const data = await requestData(`getTeams?hl=fr-FR`);
-  res.json(data);
-});
+  // #########################################################################################################################
+  // ################################################### TOURNAMENTS ROUTES ##################################################
+  // #########################################################################################################################
+  app.get('/tournaments', async (req,res) => {
+    const data = await requestData(`getCompletedEvents?hl=fr-FR`);
+    const tournaments = data.data.schedule.events;
+    res.json(tournaments);
+  });
 
-app.get('/team/:name', async (req,res) => {
-  const teamName = req.params.name;
-  const data = await requestData(`getTeams?hl=fr-FR`);
-  const teams = data.data.teams;
-  const team = teams.find(t => t.name == teamName);
-  res.json(team);
-});
+  app.get('/tournaments/league/:id', async (req,res) => {
+    const leagueId = req.params.id;
+    const data = await requestData(`getTournamentsForLeague?hl=fr-FR&leagueId=${leagueId}`);
+    res.json(data);
+  });
 
-app.get('/teams/:search', async (req,res) => {
-  const searchRequest = req.params.search;
-  const data = await requestData(`getTeams?hl=fr-FR`);
-  const teams = data.data.teams;
-  const searchResults = teams.filter(t => t.name.includes(searchRequest));
-  res.json(searchResults);
-});
-
-app.get('/teams/league/:name', async (req,res) => {
-  const leagueName = req.params.name;
-  const data = await requestData(`getTeams?hl=fr-FR`);
-  const teams = data.data.teams;
-  const teamsInLeague = teams.filter(t => t.homeLeague && t.homeLeague.name && t.homeLeague.name.includes(leagueName));
-  res.json(teamsInLeague);
-});
-
-app.get('/teams/region/:name', async (req,res) => {
-  const regionName = req.params.name;
-  const data = await requestData(`getTeams?hl=fr-FR`);
-  const teams = data.data.teams;
-  const teamsInRegion = teams.filter(t => t.homeLeague && t.homeLeague.region && t.homeLeague.region.includes(regionName));
-  res.json(teamsInRegion);
-});
-// #########################################################################################################################
+  app.get('/tournament/:id', async (req,res) => {
+    const tournamentId = req.params.id;
+    const data = await requestData(`getStandings?hl=fr-FR&tournamentId=${tournamentId}`);
+    res.json(data);
+  });
+  // #########################################################################################################################
 
 
 
-// #########################################################################################################################
-// ###################################################### MATCHS ROUTE #####################################################
-// #########################################################################################################################
-app.get('/match/:id', async (req,res) => {
-  const matchId = req.params.id;
-  const data = await requestData(`getWindow?gameId=${matchId}`);
-  res.json(data);
-});
-// #########################################################################################################################
+  // #########################################################################################################################
+  // ###################################################### TEAMS ROUTES #####################################################
+  // #########################################################################################################################
+  app.get('/teams', async (req,res) => {
+    const data = await requestData(`getTeams?hl=fr-FR`);
+    res.json(data);
+  });
+
+  app.get('/team/:name', async (req,res) => {
+    const teamName = req.params.name;
+    const data = await requestData(`getTeams?hl=fr-FR`);
+    const teams = data.data.teams;
+    const team = teams.find(t => t.name == teamName);
+    res.json(team);
+  });
+
+  app.get('/teams/:search', async (req,res) => {
+    const searchRequest = req.params.search;
+    const data = await requestData(`getTeams?hl=fr-FR`);
+    const teams = data.data.teams;
+    const searchResults = teams.filter(t => t.name.includes(searchRequest));
+    res.json(searchResults);
+  });
+
+  app.get('/teams/league/:name', async (req,res) => {
+    const leagueName = req.params.name;
+    const data = await requestData(`getTeams?hl=fr-FR`);
+    const teams = data.data.teams;
+    const teamsInLeague = teams.filter(t => t.homeLeague && t.homeLeague.name && t.homeLeague.name.includes(leagueName));
+    res.json(teamsInLeague);
+  });
+
+  app.get('/teams/region/:name', async (req,res) => {
+    const regionName = req.params.name;
+    const data = await requestData(`getTeams?hl=fr-FR`);
+    const teams = data.data.teams;
+    const teamsInRegion = teams.filter(t => t.homeLeague && t.homeLeague.region && t.homeLeague.region.includes(regionName));
+    res.json(teamsInRegion);
+  });
+  // #########################################################################################################################
 
 
 
-// #########################################################################################################################
-// #################################################### 404 ERROR ROUTE ####################################################
-// #########################################################################################################################
-app.use((req, res, next) => {
-  res.status(404).send('Sorry, this route does not exist!');
-});
-// #########################################################################################################################
+  // #########################################################################################################################
+  // ###################################################### MATCHS ROUTE #####################################################
+  // #########################################################################################################################
+  app.get('/match/:id', async (req,res) => {
+    const matchId = req.params.id;
+    const data = await requestData(`getWindow?gameId=${matchId}`);
+    res.json(data);
+  });
+  // #########################################################################################################################
 
-// Launch server on specified port
-app.listen(PORT, () => {
+
+
+  // #########################################################################################################################
+  // #################################################### 404 ERROR ROUTE ####################################################
+  // #########################################################################################################################
+  app.use((req, res, next) => {
+    res.status(404).send('Sorry, this route does not exist!');
+  });
+  // #########################################################################################################################
+
+  // Launch server on specified port
+  app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-});
+  });
 
+}
 
 // Fonction pour récupérer les données de l'API
 async function requestData(path){
